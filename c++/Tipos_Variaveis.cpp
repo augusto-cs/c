84e9e52968f02c24af3f62e232911501a9c30420
augusto <marquinaugustocruz@gmail.com>
2025-02-15 11:37:57 -0300

    primary

---
+++ c++/Tipos_Variaveis.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    system("cls");
+
+    cout<<"O que são Variaveis?";
+
+    int valorUnitario = 10;
+
+    float valorEmGrama = 3.5;
+
+    double salario = 2500.50;
+
+    char etra = 'A';
+
+    string nome= "Alexandre";
+
+    bool palmeirasNaoTemMundial = true;
+
+    valorUnitario=5;
+
+    return 0;
+}
